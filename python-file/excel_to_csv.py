a514935b9387685cb4c6d0828fd1ea6578b4ec8e
gswtmc <waku.patrick@yahoo.com>
2017-01-16 17:24:42 -0800

    Create excel_to_csv.py

---
+++ python-file/excel_to_csv.py
@@ -0,0 +1,45 @@
+'''
+Find the time and value of max load for each of the regions
+COAST, EAST, FAR_WEST, NORTH, NORTH_C, SOUTHERN, SOUTH_C, WEST
+and write the result out in a csv file, using pipe character | as the delimiter.
+'''
+import xlrd
+import os
+import csv
+
+datafile = "2013_ERCOT_Hourly_Load_Data.xls"
+outfile = "2013_Max_Loads.csv"
+
+def parse_file(datafile):
+    workbook = xlrd.open_workbook(datafile)
+    sheet = workbook.sheet_by_index(0)
+    data = {}
+    # YOUR CODE HERE
+    # Remember that you can use xlrd.xldate_as_tuple(sometime, 0) to convert
+    # Excel date to Python tuple of (year, month, day, hour, minute, second)
+    return data
+
+def save_file(data, filename):
+    # YOUR CODE HERE
+
+    
+def test():
+    data = parse_file(datafile)
+    save_file(data, outfile)
+
+    number_of_rows = 0
+    stations = []
+
+    ans = {'FAR_WEST': {'Max Load': '2281.2722140000024',
+                        'Year': '2013',
+                        'Month': '6',
+                        'Day': '26',
+                        'Hour': '17'}}
+    correct_stations = ['COAST', 'EAST', 'FAR_WEST', 'NORTH',
+                        'NORTH_C', 'SOUTHERN', 'SOUTH_C', 'WEST']
+    fields = ['Year', 'Month', 'Day', 'Hour', 'Max Load']
+
+
+if __name__ == "__main__":
+    test()
+
