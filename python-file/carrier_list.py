5f7e30e7b8c7837434229d486305364e13370e46
gswtmc <waku.patrick@yahoo.com>
2017-01-17 20:32:50 -0800

    Update carrier_list.py

--- python-file/carrier_list.py
+++ python-file/carrier_list.py
@@ -6,3 +6,21 @@ carriers.
 """
 from bs4 import BeautifulSoup
 html_page = "options.html"
+
+def extract_carriers(page):
+    data = []
+    
+    with open(page, "r") as html:
+        soup = BeautifulSoup(html, "lxml")
+        airlines = soup.find(id = "CarrierList")
+        
+        for airline in airlines.find_all("option"):
+            if len(airline['value'] == 2:
+                   data.append(airline['value'])
+                   
+    return data
+                   
+                   
+extract_carriers(html_page)
+        
+      
