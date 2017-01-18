8ab069f3771151405336826bcbff68dd37bfe33d
gswtmc <waku.patrick@yahoo.com>
2017-01-17 20:25:51 -0800

    Create scrapping_solution.py

---
+++ python-file/scrapping_solution.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+s = requests.Session()
+
+r = s.get("http://www.transtats.bts.gov/Data_Elements.aspx?Data=2")
+soup = BeautifulSoup(r.text, "lxml")
+viewstate_element = soup.find(id = "__VIEWSTATE")
+viewstate = viewstate_element["value"]
+eventvalidation_element= soup.find(id="__EVENTVALIDATION")
+eventvalidation = eventvalidation_element["value"]
+
+r = s.post("http://www.transtats.bts.gov/Data_Elements.aspx?Data=2",
+                    data={'AirportList': "BOS",
+                          'CarrierList': "VX",
+                          'Submit': 'Submit',
+                          "__EVENTTARGET": "",
+                          "__EVENTARGUMENT": "",
+                          "__EVENTVALIDATION": eventvalidation,
+                          "__VIEWSTATE": viewstate
+                    })
+
+f = open(html_page,"w")
+f.write(r.text)
